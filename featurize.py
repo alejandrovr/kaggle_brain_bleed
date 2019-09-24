4e3a3b372dc9bfab4d71f4493fb09395d4901f6e
alejandrovr <alejandrovarelarial@gmail.com>
2019-09-24 16:15:25 +0200

    Able to see images of different classes

--- featurize.py
+++ featurize.py
@@ -1,24 +1,30 @@
 import pydicom
 import dicom_numpy
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
-def extract_voxel_data(list_of_dicom_files):
-    datasets = [pydicom.read_file(f) for f in list_of_dicom_files]
-    try:
-        voxel_ndarray, ijk_to_xyz = dicom_numpy.combine_slices(datasets)
-    except dicom_numpy.DicomImportException as e:
-        # invalid DICOM data
-        raise
-    return voxel_ndarray
-
+def dcm2np(dcm_file):
+    ds = pydicom.dcmread(dcm_file)
+    im = ds.pixel_array
+    return im
 
 if __name__ == "__main__":
-    import glob
-    import matplotlib.pyplot as plt
-    dcim_files = glob.glob('/home/alejandro/kgl/rsna-intracranial-hemorrhage-detection/stage_1_train_images/ID_2a77db2aa*')
-    #['/home/alejandro/kgl/rsna-intracranial-hemorrhage-detection/stage_1_train_images/ID_2a77db2aa.dcm']
-    ds = pydicom.dcmread(dcim_files[0])
-    plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
-    plt.show()
+    #thanks to https://www.kaggle.com/marcovasquez/basic-eda-data-visualization
+    dcim_files = glob.glob('/home/alejandro/kgl/rsna-intracranial-hemorrhage-detection/stage_1_train_images/ID_2*')
+    df = pd.read_csv('/home/alejandro/kgl/rsna-intracranial-hemorrhage-detection/stage_1_train.csv')
+    df['Sub_type'] = df['ID'].str.split("_", n = 3, expand = True)[2]
+    df['PatientID'] = df['ID'].str.split("_", n = 3, expand = True)[1]
 
+    for subtype in ['any', 'epidural', 'intraparenchymal', 'intraventricular','subarachnoid', 'subdural']:
+        df_subtype = df.loc[(df['Label'] == 1) & (df['Sub_type'] == subtype)]
+        rows_subtype = df_subtype.sample(1)
+        for idx, row in rows_subtype.iterrows():
+            file_name = '/home/alejandro/kgl/rsna-intracranial-hemorrhage-detection/stage_1_train_images/ID_'+row.PatientID+'.dcm'
+            print(file_name)
+            plt.imshow(dcm2np(file_name), cmap=plt.cm.bone)
+            plt.show()
 
+        input('Next subtype?')
 
